c8852999f5786b6b2ea739f46793006c7bad0dde
Valentina Franciosi <126563681+valenfrancho@users.noreply.github.com>
2023-09-17 23:32:53 +0200

    Created models for Country and City

--- trendioexercise/geography/models.py
+++ trendioexercise/geography/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class City(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
