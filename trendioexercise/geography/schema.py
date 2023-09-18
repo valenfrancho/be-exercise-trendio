23a78b54f2d3c6c10fb8937402f66ff87bcd778e
Valentina Franciosi <126563681+valenfrancho@users.noreply.github.com>
2023-09-18 10:51:56 +0200

    implement filter and mutations

--- trendioexercise/geography/schema.py
+++ trendioexercise/geography/schema.py
@@ -2,6 +2,8 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from .models import Country, City
 
+# Defining GraphQL types:
+
 class CountryType(DjangoObjectType):
     class Meta:
         model = Country
@@ -9,6 +11,8 @@ class CountryType(DjangoObjectType):
 class CityType(DjangoObjectType):
     class Meta:
         model = City
+
+# Defining query type:
 
 class Query(graphene.ObjectType):
     all_countries = graphene.List(CountryType)
@@ -18,6 +22,49 @@ class Query(graphene.ObjectType):
         return Country.objects.all()
 
     def resolve_country_by_name(self, info, name):
-        return Country.objects.get(name=name)
+        return Country.objects.filter(name=name).first()
+    
+# Defining input types:
+    
+class CreateCountryInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
 
-schema = graphene.Schema(query=Query)
+class CreateCityInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    country_id = graphene.ID(required=True)
+
+# Implementing resolver functions:
+
+class CreateCountry(graphene.Mutation):
+    class Arguments:
+        input = CreateCountryInput(required=True)
+
+    country = graphene.Field(CountryType)
+
+    def mutate(self, info, input):
+        name = input.get('name')
+        country = Country(name=name)
+        country.save()
+        return CreateCountry(country=country)
+
+class CreateCity(graphene.Mutation):
+    class Arguments:
+        input = CreateCityInput(required=True)
+
+    city = graphene.Field(CityType)
+
+    def mutate(self, info, input):
+        name = input.get('name')
+        country_id = input.get('country_id')
+        country = Country.objects.get(pk=country_id)
+        city = City(name=name, country=country)
+        city.save()
+        return CreateCity(city=city)
+    
+# Defining mutations:
+
+class Mutation(graphene.ObjectType):
+    create_country = CreateCountry.Field()
+    create_city = CreateCity.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
